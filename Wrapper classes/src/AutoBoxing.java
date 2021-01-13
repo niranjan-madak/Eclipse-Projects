91324c788bd2390679b4052783b4ffb7367dd4cb
Er.Niranjan Madak <nmadak28@gmail.com>
2021-01-13 23:52:49 +0530

    code added

---
+++ Wrapper classes/src/AutoBoxing.java
@@ -0,0 +1,12 @@
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		Integer i=a; // Autoboxing (internally i.valueOf(a)
+		System.out.println(i);
+
+	}
+
+}
---
+++ Wrapper classes/src/UnBoxing.java
@@ -0,0 +1,12 @@
+
+public class UnBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i=new Integer(9);
+		int j=i; //unboxing
+		System.out.println(j);
+
+	}
+
+}
