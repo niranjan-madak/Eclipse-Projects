78755159a62a98a012c3f6c4c4ba55279bb95d93
Er.Niranjan Madak <nmadak28@gmail.com>
2021-01-13 23:59:01 +0530

    code added

---
+++ SPRING-06-ORM-Spring and Hibernate Integration/src/com/javatpoint/Employee.java
@@ -0,0 +1,50 @@
+package com.javatpoint;  
+  
+public class Employee {  
+private int id;  
+private String name;  
+private float salary;
+/**
+ * @return the id
+ * 
+ * 
+ */
+public Employee() {}
+public int getId() {
+	return id;
+}
+/**
+ * @param id the id to set
+ */
+public void setId(int id) {
+	this.id = id;
+}
+/**
+ * @return the name
+ */
+public String getName() {
+	return name;
+}
+/**
+ * @param name the name to set
+ */
+public void setName(String name) {
+	this.name = name;
+}
+/**
+ * @return the salary
+ */
+public float getSalary() {
+	return salary;
+}
+/**
+ * @param salary the salary to set
+ */
+public void setSalary(float salary) {
+	this.salary = salary;
+}  
+  
+//getters and setters 
+
+  
+}  
