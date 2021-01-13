379a15917dc3d92954ed435ce0f197f3de16c2a6
Niranjan Madak <nmadak28@gmail.com>
2021-01-13 22:14:45 +0530

    Hello class is added

---
+++ Hello_World/src/com/madak/Hello.java
@@ -0,0 +1,11 @@
+package com.madak;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"Hi welcome this is the overview to see how we can add project from eclipse to local as well as remote repo ...Xp");
+
+	}
+
+}
