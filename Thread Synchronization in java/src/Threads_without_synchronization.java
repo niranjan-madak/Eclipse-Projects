4e269f355539785b12bc70ad047fb86bcebbc30e
Er.Niranjan Madak <nmadak28@gmail.com>
2021-01-13 23:50:55 +0530

    code added

---
+++ Thread Synchronization in java/src/Threads_without_synchronization.java
@@ -0,0 +1,46 @@
+class Table1{
+        void printTable(int n) {
+		for(int i=1;i<=5;i++) {
+			System.out.println(n*i);
+			try {
+				Thread.sleep(400);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+class MyThread3 extends Thread{
+	Table1 t;
+	MyThread3(Table1 t){
+		this.t=t;
+	}
+	public void run() {
+		t.printTable(5);
+	}
+}
+class MyThread4 extends Thread{
+	Table1 t;
+	MyThread4(Table1 t){
+	this.t=t;
+	}
+	public void run() {
+		t.printTable(10);
+	}
+}
+public class Threads_without_synchronization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Table1 obj=new Table1();
+		MyThread3 t1=new MyThread3(obj);
+		MyThread4 t2=new MyThread4(obj);
+		t1.start();
+	
+		t2.start();
+
+	}
+
+}
